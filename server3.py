d839b6562e0dbf2ed0b1855b671a9b709cafa829
gr8geek <pratyush9000@gmail.com>
2020-11-07 12:24:52 +0530

    Made the changes

--- server3.py
+++ server3.py
@@ -12,16 +12,34 @@ feeds = {
     "zee":ZEE_NEWS
 }
 
-@app.route("/")
+@app.route("/",methods  = ["GET","POST"])
 def get_news():
-    pub = request.args.get("publication")
-    if pub==None:
-        return "<h1>Not Found</h1>"
+    if request.method == "POST":
+        pub = request.form.get("pub")
+        if pub=='':
+                feed = feedparser.parse(feeds['ndtv'])
+                pub = "ndtv"
+                return render_template("news.html",channel = pub.upper()+" NEWS", news = feed["entries"])
 
-    pub = pub.lower()
-    if feeds[pub]:
-            feed = feedparser.parse(feeds[pub])
-            return render_template("news.html",channel = pub.upper()+" NEWS", news = feed["entries"])
-    return "<h1>Not Found</h1>"
+
+        pub = pub.lower()
+        if feeds[pub]:
+                feed = feedparser.parse(feeds[pub])
+                return render_template("news.html",channel = pub.upper()+" NEWS", news = feed["entries"])
+        return "<h1>Not Found</h1>"
+    if request.method =="GET":
+        print("---------get method -----------")
+        pub = request.args.get("publication")
+        if pub==None:
+            feed = feedparser.parse(feeds['ndtv'])
+            pub = 'ndtv'
+        else:
+            feed = feedparser.parse(feeds[pub.lower()])
+        return render_template("news.html",channel = pub.upper()+" NEWS", news = feed["entries"])
+        pub = pub.lower()
+        if feeds[pub]:
+                feed = feedparser.parse(feeds[pub])
+                return render_template("news.html",channel = pub.upper()+" NEWS", news = feed["entries"])
+        return "<h1>Not Found</h1>"
 
 app.run(debug=True)
